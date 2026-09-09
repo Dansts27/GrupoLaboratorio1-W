082032288fbda5fb3b99cb7a920861f8d219abf5
Dansts27 <00101926@uca.edu.sv>
2026-09-09 15:01:42 -0600

    feat:añadiendo funciones
    Se implementan las funciones iniciales del programa para consultar las calorías actuales y agregar nuevos registros de consumo.

--- calorias_diarias.cc
+++ calorias_diarias.cc
@@ -1,6 +1,33 @@
 #include <iostream>
 
+//variables
+float calorias_totales = 2000;
+float cant;
+
+//Puntero
+float *pCalorias = &calorias_totales;
+
+//Funciones
+void ConsultarCalorias (float x);
+void AgregarCalorias (float &x, float y);
+
 int main () {
-    
+
     return 0;
+}
+
+void ConsultarCalorias (float x) {
+std::cout<<"\nCalorias totales: " <<x;
+}
+
+void AgregarCalorias (float &x, float y) {
+std::cout<<"\nCalorias a agregar: ";
+std::cin>>y;
+if (y>=0) {
+x += y;
+std::cout<<"\nOperacion realizada con exito.";
+}
+else {
+    std::cout<<"ERROR.\nLa cantidad ingresada no es valida.";
+}  
 }
