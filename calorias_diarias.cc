9ac7cade7ef2974edb2eb0058c84909f8e2ac6cf
RodrigoAguin <00172426@uca.edu.sv>
2026-09-09 15:29:33 -0600

    feat: agregar funcion para quemar calorias

--- calorias_diarias.cc
+++ calorias_diarias.cc
@@ -1,6 +1,34 @@
 #include <iostream>
 
+struct DatosUsuario {
+    float calorias;
+    float cant;
+};
+
+DatosUsuario usuario;
+
+void QuemarCalorias(float *x, float y);
+
 int main () {
     
+    usuario.calorias = 2000;
+    
     return 0;
+}
+
+void QuemarCalorias(float *x, float y)
+{
+    std::cout << "Calorias quemadas: ";
+    std::cin >> y;
+    
+    if(y > 0 && y <= *x){
+
+        *x -= y;
+
+        std::cout << "\n--Operacion realizada exitosamente";
+    }
+    else
+    {
+        std::cout << "\n--*ERROR: el valor ingresado no es valido*";
+    }
 }
